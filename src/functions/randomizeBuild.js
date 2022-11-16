ad1957a9a631622c987db81db802814ccfc16694
Ezkrman <tony.almonacid@hotmail.com>
2022-11-16 17:02:52 -0500

    Added new items from pre-season 13

--- src/functions/randomizeBuild.js
+++ src/functions/randomizeBuild.js
@@ -84,7 +84,7 @@ const drawRandomBuild = (championObject) => {
     drewItems.push(copiedBootsArray[randomBootIndex]);
   }
 
-  const randomMythicIndex = getRandomIndex(25);
+  const randomMythicIndex = getRandomIndex(29);
   const drewMythic = copiedMythicArray[randomMythicIndex];
 
   drewItems.push(drewMythic);
